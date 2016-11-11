739b23a7442a88939d07536b59d7dc1005e6198a
wilsontom <wilsontom89@gmail.com>
2016-11-11 15:47:43 +0000

    quanitication method

---
+++ R/quantify-method.R
@@ -0,0 +1,33 @@
+
+
+#' @include allGenerics.R
+#' @include allClasses.R
+
+setMethod(f = "quantify", signature = "skyline",
+          function(object){
+            objectName <- as.list(match.call())$object
+            cls_idx <- which(object@runinfo$class != "calibrant")
+
+            quant <- NULL
+            for(i in seq_along(cls_idx)){
+                dftmp <- data.frame(id = object@peakInfo$area[,1],
+                                  area = object@peakInfo$area[cls_idx[i] + 1])
+
+                                cidx <- match(dftmp[,"id"],object@calibrants[,"id"])
+                quant[[i]] <- (dftmp[,2] - object@calibrants[cidx,"c"]) / object@calibrants[cidx,"m"]
+              }
+
+            result_lyt_qn <- data.frame(matrix(ncol = length(quant) + 1, nrow = length(quant[[1]])))
+
+            names(result_lyt_qn) <- c("id",object@runinfo[cls_idx,"name"])
+
+            result_lyt_qn[,"id"] <- object@calibrants[cidx,"id"]
+
+           for(i in seq_along(quant)){
+                result_lyt_qn[,i + 1] <- quant[[i]]
+            }
+
+          object@quant <- result_lyt_qn
+          assign(eval(paste(text = objectName)),object, envir = .GlobalEnv)
+      }
+)
