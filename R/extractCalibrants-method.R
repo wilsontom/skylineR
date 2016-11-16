2ff2510b2532b1d5ce2058dd2056ec7da82f9278
wilsontom <wilsontom89@gmail.com>
2016-11-16 20:55:50 +0000

    new method for handling calibrants

---
+++ R/extractCalibrants-method.R
@@ -0,0 +1,38 @@
+
+
+# find out which files are calibrants first
+
+object <- sky
+
+extractCalibrants <- function(object)
+	{
+
+calibrant_id <- which(object@runinfo[,"class"] == "calibrant")
+
+
+calibrant_area <- data.frame(object@peakInfo$area[,calibrant_id + 1])
+rownames(calibrant_area) <- object@peakInfo$area[,"id"]
+
+names(calibrant_area) <- object@runinfo[calibrant_id,"name"]
+
+## order
+
+calibrant_split <- split(calibrant_area, rownames(calibrant_area))
+
+calibrant_df <- lapply(calibrant_split, t)
+
+for(i in seq_along(calibrant_df)){
+	calibrant_df[[i]] <- data.frame(calibrant_df[[i]], object@runinfo[calibrant_id,"conc"])
+	names(calibrant_df[[i]]) <- c("area", "conc")
+}
+
+IS_area <- calibrant_df$IS1[,"area"]
+
+
+for(i in seq_along(calibrant_df)){
+	calibrant_df[[i]] <- data.frame(calibrant_df[[i]],ratio = calibrant_df[[i]][,"area"] / IS_area)
+}
+object@calibrants <- calibrant_df
+
+return(object)
+}
