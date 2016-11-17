76f2f9b863911065ad76005dbbd810f1cc58ea12
wilsontom <wilsontom89@gmail.com>
2016-11-17 18:58:39 +0000

    clean up

--- R/calibrate-method.R
+++ R/calibrate-method.R
@@ -9,44 +9,28 @@
 setMethod(f = "calibrate", signature = "skyline",
           function(object){
 
-            objectName <- as.list(match.call())$object
-            caIdx <- which(object@runinfo[,"class"] == "calibrant")
-            caPA <- data.frame(id = object@peakInfo$area[,1], object@peakInfo$area[,caIdx + 1])
+          objectName <- as.list(match.call())$object
+				  cali_tmp <- NULL
+	       for(i in seq_along(object@calibrants)){
+			          cali_tmp[[i]] <- data.frame(x = object@calibrants[[i]][,"conc"],y = object@calibrants[[i]][,"ratio"])
+	        }
 
-            names(caPA) <- c("id", names(object@peakInfo$area[,caIdx + 1]))
+  	     names(cali_tmp) <- names(object@calibrants)
+			   calibration_optimise <- lapply(cali_tmp, linear_opt)
 
-            caPA2 <- split(caPA, as.numeric(rownames(caPA)))
-            caPA3 <- lapply(caPA2, t)
+          calib_df <- data.frame(matrix(nrow = length(calibration_optimise), ncol = 7))
+          names(calib_df) <- c("id", "R2", "c", "m", "n", "err", "range")
+          for(i in seq_along(calibration_optimise)){
+            calib_df[i,"id"] <- names(calibration_optimise)[i]
+            calib_df[i,"R2"] <- calibration_optimise[[i]]$r2
+            calib_df[i,"c"] <- calibration_optimise[[i]]$c
+            calib_df[i,"m"] <- calibration_optimise[[i]]$m
+            calib_df[i,"n"] <- calibration_optimise[[i]]$pts
+            calib_df[i,"err"] <- calibration_optimise[[i]]$error
+			      calib_df[i,"range"] <- calibration_optimise[[i]]$quant_range
+          }
 
-            caDF <- NULL
-            for(i in seq_along(caPA3)){
-              names(caPA3)[i] <- caPA3[[i]]["id",]
-              caPA3[[i]] <- caPA3[[i]][-1,]
-
-              names(caPA3[[i]]) <- gsub("\\.", "-", names(caPA3[[i]]))
-              idm <- match(names(caPA3[[i]]), object@runinfo[,"name"])
-
-              caDF[[i]] <- data.frame(conc = object@runinfo[idm,"conc"], value = as.numeric(caPA3[[i]]))
-              caDF[[i]] <- caDF[[i]][order(caDF[[i]][,"conc"]),]
-            }
-
-            names(caDF) <- names(caPA3)
-            caDF$IS <- NULL
-            calibration_optimise <- lapply(caDF, linear_opt)
-
-            calib_df <- data.frame(matrix(nrow = length(calibration_optimise), ncol = 6))
-            names(calib_df) <- c("id", "R2", "c", "m", "n", "err")
-            for(i in seq_along(calibration_optimise)){
-              calib_df[i,"id"] <- names(calibration_optimise)[i]
-              calib_df[i,"R2"] <- calibration_optimise[[i]]$r2
-              calib_df[i,"c"] <- calibration_optimise[[i]]$c
-              calib_df[i,"m"] <- calibration_optimise[[i]]$m
-              calib_df[i,"n"] <- calibration_optimise[[i]]$pts
-              calib_df[i,"err"] <- calibration_optimise[[i]]$error
-            }
-
-            object@calibrants <- calib_df
-
-            assign(eval(paste(text = objectName)),object, envir = .GlobalEnv)
+          object@calibration <- calib_df
+          assign(eval(paste(text = objectName)),object, envir = .GlobalEnv)
           }
 )
