f64e283cf0f212857db147d79308c888cb281af1
wilsontom <wilsontom89@gmail.com>
2016-11-18 15:24:05 +0000

    update

--- R/calibrate-method.R
+++ R/calibrate-method.R
@@ -16,18 +16,17 @@ setMethod(f = "calibrate", signature = "skyline",
 	        }
 
   	     names(cali_tmp) <- names(object@calibrants)
-			   calibration_optimise <- lapply(cali_tmp, linear_opt)
 
-          calib_df <- data.frame(matrix(nrow = length(calibration_optimise), ncol = 7))
-          names(calib_df) <- c("id", "R2", "c", "m", "n", "err", "range")
+			   calibration_optimise <- lapply(cali_tmp, curveOptimise)
+
+			   calib_df <- data.frame(matrix(nrow = length(calibration_optimise), ncol = 5))
+          names(calib_df) <- c("id", "Rsq", "n", "err", "range")
           for(i in seq_along(calibration_optimise)){
             calib_df[i,"id"] <- names(calibration_optimise)[i]
-            calib_df[i,"R2"] <- calibration_optimise[[i]]$r2
-            calib_df[i,"c"] <- calibration_optimise[[i]]$c
-            calib_df[i,"m"] <- calibration_optimise[[i]]$m
-            calib_df[i,"n"] <- calibration_optimise[[i]]$pts
-            calib_df[i,"err"] <- calibration_optimise[[i]]$error
-			      calib_df[i,"range"] <- calibration_optimise[[i]]$quant_range
+            calib_df[i,"Rsq"] <- as.numeric(calibration_optimise[[i]][["rsq"]])
+            calib_df[i,"n"] <- as.numeric(calibration_optimise[[i]][["pts"]])
+            calib_df[i,"err"] <- as.numeric(calibration_optimise[[i]][["error"]])
+			      calib_df[i,"range"] <- calibration_optimise[[i]][["quant_range"]]
           }
 
           object@calibration <- calib_df
