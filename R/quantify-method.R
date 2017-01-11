81c9fa284a1b78abafdb5b9802c1071e0fd068b8
wilsontom <wilsontom89@gmail.com>
2017-01-11 16:59:45 +0000

    allow for raw areas to be used

--- R/quantify-method.R
+++ R/quantify-method.R
@@ -7,7 +7,7 @@
 #' @include allClasses.R
 
 setMethod(f = "quantify", signature = "skyline",
-          function(object){
+          function(object, area){
             objectName <- as.list(match.call())$object
             cls_idx <- which(object@runinfo$class != "calibrant")
 
@@ -17,18 +17,31 @@ setMethod(f = "quantify", signature = "skyline",
 			
 				tmp <- object@peakInfo$area[i,cls_idx + 1]
 				rownames(tmp) <- object@peakInfo$area[i,1]
-			
-				is_match <- match(names(tmp), rownames(object@internalstd))
-				tmpR  <- tmp / object@internalstd[is_match,"area"]
-			
-				cidx <- match(rownames(tmpR),object@calibration[[1]][,"id"])
-			
-				calMod <-  fitCurve(object@calibrants[[cidx]][object@calibration[[2]][[cidx]],"conc"],
-											object@calibrants[[cidx]][object@calibration[[2]][[cidx]],"ratio"], type = "Quad-LogLog")
 				
+				if(area == "ratio"){
+					is_match <- match(names(tmp), rownames(object@internalstd))
+					tmpR  <- tmp / object@internalstd[is_match,"area"]
+			
+					cidx <- match(rownames(tmpR),object@calibration[[1]][,"id"])
+			
+					calMod <-  fitCurve(object@calibrants[[cidx]][object@calibration[[2]][[cidx]],"conc"],
+							object@calibrants[[cidx]][object@calibration[[2]][[cidx]],"ratio"], 
+							type = object@calibration[[1]][cidx,"function"])
+			}
+				if(area == "raw"){
+					tmpR <- tmp
+
+					cidx <- match(rownames(tmpR),object@calibration[[1]][,"id"])
+			
+					calMod <-  fitCurve(object@calibrants[[cidx]][object@calibration[[2]][[cidx]],"conc"],
+							object@calibrants[[cidx]][object@calibration[[2]][[cidx]],"area"], 
+							type = object@calibration[[1]][cidx,"function"])
+	
+			
+			}
 				
 				for_pred <- data.frame(t(tmpR))
-				
+
 				
 				quant_x[[i]] <- predictModel(calMod, for_pred[,1])
 				}
