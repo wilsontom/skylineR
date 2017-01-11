8f7b6a5bccf0d59bc765454cdff5a0ea2c854663
wilsontom <wilsontom89@gmail.com>
2017-01-11 10:53:34 +0000

    fix bug for single/multi IS transitions

--- R/extractIS.R
+++ R/extractIS.R
@@ -15,11 +15,17 @@ setMethod(f = "extractIS", signature = "skyline",
 
 		ISidx <- which(object@peakInfo$area[,"id"] == "IS")
 	
-		row_lables <- names(object@peakInfo$area[ISidx,-1])
-		ISarea <- object@peakInfo$area[ISidx,-1]
-		ISrt <- object@peakInfo$Rt[ISidx,-1]
-		ISnoise <- object@peakInfo$noise[ISidx,-1]
-
+		if(length(ISidx) == 1){
+			row_lables <- names(object@peakInfo$area[ISidx,-1])
+			ISarea <- t(object@peakInfo$area[ISidx,-1])
+			ISrt <- t(object@peakInfo$Rt[ISidx,-1])
+			ISnoise <- t(object@peakInfo$noise[ISidx,-1])
+		}else{
+			row_lables <- names(object@peakInfo$area[ISidx,-1])
+			ISarea <- object@peakInfo$area[ISidx,-1]
+			ISrt <- object@peakInfo$Rt[ISidx,-1]
+			ISnoise <- object@peakInfo$noise[ISidx,-1]
+		}
 		if(length(ISidx) > 1){		
 			ISarea <- apply(ISarea,2,sum)
 			ISrt <- apply(ISrt,2,mean)
@@ -28,6 +34,7 @@ setMethod(f = "extractIS", signature = "skyline",
 			
 	
 		ISdf <- data.frame(area = ISarea, noise = ISnoise, Rt = ISrt)
+		names(ISdf) <- c("area", "noise", "rt")
 		rownames(ISdf) <- row_lables
 		object@internalstd <- ISdf
 
