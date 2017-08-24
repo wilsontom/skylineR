64c6b5426e16a5c6e9f2b852e6d5714399a78473
wilsontom <wilsontom89@gmail.com>
2017-08-24 14:52:47 +0100

    rename

---
+++ R/add_path-method.R
@@ -0,0 +1,22 @@
+#' add_path
+#' @rdname add_path
+#'
+#' @description Add a `path` location which will be used as a temporay save to write-out `Skyline` result files
+#'
+#' @include allGenerics.R
+#' @include allClasses.R
+
+
+setMethod(f = "add_path", signature = "skyline",
+          function(object, path){
+
+            objectName <- as.list(match.call())$object
+
+            if(!isTRUE(dir.exists(path))){
+              stop(deparse(substitute(path)), " must be an exisiting directory path", call. = FALSE)
+            }
+
+            object@path <- path
+            assign(eval(paste(text = objectName)),object, envir = .GlobalEnv)
+          }
+)
